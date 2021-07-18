c7bb3dc017806760ca060340df6726fb83bc7832
EH-GD-MOHIT21 <70685094+EH-GD-MOHIT21@users.noreply.github.com>
2021-07-18 14:29:23 +0530

    Update settings.py

--- gform/settings.py
+++ gform/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y8^-=sykwrl#8h(fu#d3dtg_(2$z@ips1&9lnl@%(q3_vndxbc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','nestedform.herokuapp.com']
 
